b13d22480f6f96b31f2a526d76fc388d5011cdce
Simon Olofsson <s.olofsson@gmail.com>
2016-09-15 21:55:14 +0200

    Angular controller generator can now handle foreign key relationships for setPutState() functions.

--- generators/angular/js/AngularJSAdminControllerGenerator.py
+++ generators/angular/js/AngularJSAdminControllerGenerator.py
@@ -1,5 +1,6 @@
 from generators.angular.js.AngularJSGenerator import AngularJSGenerator
 from generators.angular.js.JavaScriptFunction import JavaScriptFunction
+from readers.ForeignKey import ForeignKey
 
 
 class AngularJSAdminControllerGenerator(AngularJSGenerator):
@@ -7,6 +8,7 @@ class AngularJSAdminControllerGenerator(AngularJSGenerator):
     def __init__(self, data, output_directory):
         super().__init__(data, output_directory)
         self.columns = data['columns']
+        self.foreign_keys = data['foreign keys']
         self.dependencies = ["$scope", "$rootScope", "$http", "$filter", "SendObjectService"]
 
     def construct_name(self):
@@ -18,17 +20,43 @@ class AngularJSAdminControllerGenerator(AngularJSGenerator):
     def construct_object_to_be_sent(self):
         return "$scope.{table_name}ToBeSent = {{}}".format(table_name=self.table_name)
 
-    def construct_set_put_state_function(self):
+    def construct_select_foreign_key_object_functions(self, parameter):
+
+        select_foreign_key_functions = []
+
+        if self.foreign_keys is not None:
+            for foreign_key in self.foreign_keys:
+                foreign_table = foreign_key.foreign_table.capitalize()
+                function_name = "var select{foreign_table}".format(foreign_table=foreign_table)
+                body = "angular.forEach($scope.{foreign_table}, function(value) {{if(value.{foreign_table_pk} == {parameter}){{$scope.selected{foreign_table} = jQuery.extend({{}}, value);}}}});".format(foreign_table=foreign_table,
+                                                                                                                                                                                                      foreign_table_pk=foreign_key.foreign_column,
+                                                                                                                                                                                                      parameter=parameter)
+
+                function = JavaScriptFunction(function_name, parameter, body)
+                function.add_useful_call_parameter(foreign_key.key)
+
+                select_foreign_key_functions.append(function)
+
+        return select_foreign_key_functions
+
+    def construct_set_put_state_function(self, select_foreign_key_object_functions):
+
         function_name = "$scope.setPutState"
         parameters = self.table_name
 
         body = ""
+
         for column in self.columns:
             body += "$scope.{table_name}ToBeSent.{column} = {table_name}.{column};".format(table_name=self.table_name,
                                                                                            column=column)
+        for select_foreign_key_object_function in select_foreign_key_object_functions:
+            call_parameter = self.table_name + "." + select_foreign_key_object_function.get_useful_call_parameter()
+            body += select_foreign_key_object_function.get_function_call(call_parameter) + ";"
 
-        js_function = JavaScriptFunction(function_name, parameters, body)
-        function = js_function.retrieve()
+        body += "$scope.sendGig = $scope.putGig;"
+
+        function = JavaScriptFunction(function_name, parameters, body)
+        return function.retrieve()
 
     def construct_set_post_state_function(self):
         pass
@@ -53,4 +81,6 @@ class AngularJSAdminControllerGenerator(AngularJSGenerator):
         controller_name = self.construct_name()
         dependencies = self.construct_dependencies()
         object_to_be_sent = self.construct_object_to_be_sent()
-        set_put_state_function = self.construct_set_put_state_function()
+        select_foreign_key_object_functions = self.construct_select_foreign_key_object_functions(self.table_name)
+        set_put_state_function = self.construct_set_put_state_function(select_foreign_key_object_functions)
+        print(set_put_state_function)
--- generators/angular/js/JavaScriptFunction.py
+++ generators/angular/js/JavaScriptFunction.py
@@ -7,6 +7,8 @@ class JavaScriptFunction():
         self.function_name = function_name
         self.parameters = parameters
         self.function_body = function_body
+        self.call_parameter = ""
+        self.first_parameter = True
 
     def retrieve(self):
         function = "{name} = function({parameters}) {{{body}}}".format(name=self.function_name,
@@ -17,3 +19,22 @@ class JavaScriptFunction():
         function = formatter.prettify(function)
 
         return function
+
+    def get_function_call(self, parameters):
+        function_signature = self.function_name.split("=")[0]
+        function_signature = function_signature.split(" ")[1]
+        function_signature += "({parameters})".format(parameters=parameters)
+
+        return function_signature
+
+    def add_useful_call_parameter(self, parameter):
+
+        if self.first_parameter == True:
+            self.first_parameter = False
+        else:
+            self.call_parameter += ", "
+
+        self.call_parameter += parameter
+
+    def get_useful_call_parameter(self):
+        return self.call_parameter
